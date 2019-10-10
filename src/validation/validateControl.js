25d32ae355623a5b4115ed60905a5d2264a1abcb
Aslak Hollund <aslakhol@gmail.com>
2019-10-11 00:28:19 +0200

    refactor parts of validateControl into utils

--- src/validation/validateControl.js
+++ src/validation/validateControl.js
@@ -1,57 +1,16 @@
+import {
+  destructFodselsnummer,
+  calculateFirstControl,
+  calculateSecondControl
+} from "./utils";
+
 export const isValidControl = fodselsnummer => {
   const fnArray = destructFodselsnummer(fodselsnummer);
   return isValidFirstControl(fnArray) && isValidSecondControl(fnArray);
 };
 
-const destructFodselsnummer = fn => {
-  return fn.split("").map(Number);
-};
-
 const isValidFirstControl = fodselsnummer => {
   return calculateFirstControl(fodselsnummer) === fodselsnummer[9];
-};
-
-const calculateFirstControl = fn => {
-  const weightedSum =
-    3 * fn[0] +
-    7 * fn[1] +
-    6 * fn[2] +
-    1 * fn[3] +
-    8 * fn[4] +
-    9 * fn[5] +
-    4 * fn[6] +
-    5 * fn[7] +
-    2 * fn[8];
-
-  const rest = weightedSum % 11;
-
-  if (rest === 0) {
-    return rest;
-  } else {
-    return 11 - rest;
-  }
-};
-
-const calculateSecondControl = fn => {
-  const weightedSum =
-    5 * fn[0] +
-    4 * fn[1] +
-    3 * fn[2] +
-    2 * fn[3] +
-    7 * fn[4] +
-    6 * fn[5] +
-    5 * fn[6] +
-    4 * fn[7] +
-    3 * fn[8] +
-    2 * fn[9];
-
-  const rest = weightedSum % 11;
-
-  if (rest === 0) {
-    return rest;
-  } else {
-    return 11 - rest;
-  }
 };
 
 const isValidSecondControl = fodselsnummer => {
