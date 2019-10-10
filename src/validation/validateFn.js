8312f71955fbec519fd3592325fb7180d4979e5e
Aslak Hollund <aslakhol@gmail.com>
2019-10-10 23:10:52 +0200

    finish validate control

--- src/validation/validateFn.js
+++ src/validation/validateFn.js
@@ -11,7 +11,70 @@ import { isValidDob } from "./validateDob";
 // 1940-1999, brukes også serien 999-900
 // 2000-2039, brukes serien 999-500
 
+export const isValidControl = fodselsnummer => {
+  const fnArray = destructFodselsnummer(fodselsnummer);
+  return isValidFirstControl(fnArray) && isValidSecondControl(fnArray);
+};
+
+const destructFodselsnummer = fn => {
+  return fn.split("").map(Number);
+};
+
+const isValidFirstControl = fodselsnummer => {
+  return calculateFirstControl(fodselsnummer) === fodselsnummer[9];
+};
+
+const calculateFirstControl = fn => {
+  const weightedSum =
+    3 * fn[0] +
+    7 * fn[1] +
+    6 * fn[2] +
+    1 * fn[3] +
+    8 * fn[4] +
+    9 * fn[5] +
+    4 * fn[6] +
+    5 * fn[7] +
+    2 * fn[8];
+
+  const rest = weightedSum % 11;
+
+  if (rest === 0) {
+    return rest;
+  } else {
+    return 11 - rest;
+  }
+};
+
+const calculateSecondControl = fn => {
+  const weightedSum =
+    5 * fn[0] +
+    4 * fn[1] +
+    3 * fn[2] +
+    2 * fn[3] +
+    7 * fn[4] +
+    6 * fn[5] +
+    5 * fn[6] +
+    4 * fn[7] +
+    3 * fn[8] +
+    2 * fn[9];
+
+  const rest = weightedSum % 11;
+
+  if (rest === 0) {
+    return rest;
+  } else {
+    return 11 - rest;
+  }
+};
+
+const isValidSecondControl = fodselsnummer => {
+  return calculateSecondControl(fodselsnummer) === fodselsnummer[10];
+};
+
 const isValidFn = fnString => {
+  if (fnString.length !== 11) {
+    return false;
+  }
   const dob = fnString.substring(0, 6);
   const individ = fnString.substring(6, 9);
   const control = fnString.substring(9, 12);
@@ -22,9 +85,12 @@ const isValidFn = fnString => {
 };
 
 export const isValidIndivid = individ => {
-  return false;
-};
-
-const isValidControl = control => {
-  return false;
+  if (individ.length !== 3 || isNaN(individ)) {
+    return false;
+  } else {
+    return true;
+  }
+  // I considered validating that the number fell into the correct range based on year,
+  // However, as we just have the last two digits in the year the ranges overlapped so much
+  // That it lost most of it's purpose. So I skipped it.
 };
--- src/validation/validateFn.test.js
+++ src/validation/validateFn.test.js
@@ -1,5 +1,30 @@
-import { isValidIndivid } from "./validateFn";
+import { isValidIndivid, isValidControl } from "./validateFn";
 
-it("returns false", () => {
-  expect(isValidIndivid(2)).toBe(false);
+describe("isValidIndivid", () => {
+  it("returns false with too long number", () => {
+    expect(isValidIndivid(1337)).toBe(false);
+  });
+  it("returns false with non number input", () => {
+    expect(isValidIndivid("foo")).toBe(false);
+  });
 });
+
+describe("isValidControl", () => {
+  it("returns true with valid fodselsnummer", () => {
+    expect(isValidControl("03079442703")).toBe(true);
+  });
+  it("returns true with valid fodselsnummer", () => {
+    expect(isValidControl("13039334156")).toBe(true);
+  });
+  it("returns false with invalid fodselsnummer", () => {
+    expect(isValidControl("03079455743")).toBe(false);
+  });
+  it("returns false with invalid fodselsnummer", () => {
+    expect(isValidControl("13039335159")).toBe(false);
+  });
+  it("returns false with strings in fodselsnummer", () => {
+    expect(isValidControl("13039s35s59")).toBe(false);
+  });
+});
+
+// console.log("1:", weightedSum, weightedSum % 11, 11 - (weightedSum % 11), fn);
