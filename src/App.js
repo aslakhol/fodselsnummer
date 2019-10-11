69ebc61cce58caebde4bdfee8b8fbceb52b63fe0
Aslak Hollund <aslakhol@gmail.com>
2019-10-11 03:23:47 +0200

    veileder woop woop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "./App.css";
+import Veileder from "nav-frontend-veileder";
 import Fodselsnummer from "./Fodselsnummer";
+import veilederSvg from "./veileder.svg";
 
 function App() {
   return (
     <div className="App">
       <div className="wrapper">
+        <Veileder
+          tekst={
+            <span>
+              Her kan du skrive inn fødselsnummeret ditt for å sjekke om du er
+              en ekte person!
+            </span>
+          }
+        >
+          <img src={veilederSvg} alt="veileder" />
+        </Veileder>
         <Fodselsnummer />
       </div>
     </div>
